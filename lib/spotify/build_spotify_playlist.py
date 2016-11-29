24580f74dee92da38d4a19370a28f0914fc3b63f
Nadav Recca <nadav@blendlabs.com>
2016-11-29 15:41:09 -0500

    filter

--- lib/spotify/build_spotify_playlist.py
+++ lib/spotify/build_spotify_playlist.py
@@ -21,10 +21,21 @@ class Track(object):
         return self.id + self.name + self.artists
 
 
+def filter_search_terms(search_term):
+    """Spotify API doesn't seem to like it when you search for a logical term
+    see https://github.com/spotify/web-api/issues/368
+    """
+    badwords = ['not', 'and', 'or', 'if']
+    return [word for word in search_term if word.lower() not in badwords]
+
+
 def get_tracks_from_recommendations(recommendations):
+    """get one spotify track per recommendation from a list of recommendations
+    """
     sp = spotipy.Spotify()
     tracks = []
     for rec in recommendations:
+        rec = filter_search_terms(rec)
         logger.info('searching for %s', rec)
         res = sp.search(rec, type='track')['tracks']['items']
         if res:
